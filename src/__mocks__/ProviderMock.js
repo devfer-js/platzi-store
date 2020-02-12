3fc2c2c306f633e675ab745fa9b4df778a8d7fc3
devfer <ferkki@hotmail.com>
2020-02-12 06:33:42 -0400

    mocks implementados

---
+++ src/__mocks__/ProviderMock.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createStore } from 'redux';
+import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createBrowserHistory } from 'history';
+import initialState from '../initialState';
+
+const store = createStore(reducer, initialState);
+const history = createBrowserHistory();
+
+const ProviderMock = props => (
+  <Provider store={store}>
+    <Router history={history}>{props.children}</Router>
+  </Provider>
+);
